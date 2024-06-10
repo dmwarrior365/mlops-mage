cb20d90f982b5b59663744a6a09c8d26e6f11ef7
Redot's Learn <74037003+dmwarrior365@users.noreply.github.com>
2024-06-10 17:23:39 +0000

    New file for submit homework 3

---
+++ homework_3.sh
@@ -0,0 +1,11 @@
+# Question 1
+  Version of current Mage is v0.9.71
+# Question 2
+  The first lines of new project is 55 rows
+# Question 3
+  The Yellow Taxi on March 2023, I received 3.403.766 rows data.
+# Question 4
+  After applying a filter to the datasets earlier, I received 3.316.216 rows data.
+# Question 5
+
+# Question 6
